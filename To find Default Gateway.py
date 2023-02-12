9a956eb5096ac80d01f286750d504456e9287477
NINJAX3X <125155323+NINJAX3X@users.noreply.github.com>
2023-02-12 11:06:47 +0530

    Default gateway to use in a script to config HPE switches
    In this you will be able to set a pattern for default gateway in a large LAN network and use it in a script to configure the default gateway in the switches.

--- To find Default Gateway.py
+++ To find Default Gateway.py
@@ -1,5 +1,7 @@
 import sys
 
+print("\n")
+
 t_o = input("Enter 3rd octet of IP address:  ")
 
 print("\n")
@@ -8,12 +10,15 @@ f_o = input("Enter 4th octet of IP address:  ")
 
 print("\n")
 
-if int(f_o) <= 126:
+if 2 <= int(f_o) <= 126:
     print ("Gateway will be 1")
     x = '.1'
-else:
+elif 128 <= int(f_o) <= 254:
     print ("Gatewy will be 129")
     x = '.129'
+else:
+    print("Define a valid octet between (1-254)")
+    sys.exit()
 
 def_gat = '10.15.'+str(t_o)+str(x)
 
@@ -21,9 +26,8 @@ output = def_gat
 
 print("\n")
 
-print("The outpur is: " ,output)
+print("The output is: " ,output)
 sys.exit()
-
 
 
 
