45d3a4ca06bdc31794445e34bea0b58e019dc9e7
NINJAX3X <125155323+NINJAX3X@users.noreply.github.com>
2023-09-23 17:22:00 +0530

    Update Configuring SNMPv3 on Aruba 2530F and 2930F switches.py

--- Configuring SNMPv3 on Aruba 2530F and 2930F switches.py
+++ Configuring SNMPv3 on Aruba 2530F and 2930F switches.py
@@ -1,18 +1,23 @@
 import netmiko
 from netmiko import ConnectHandler
 import sys
-import time
+
+#Inorder to run this script you need to configure few things first through console.
 
 aruba_osswitch = {
                     "device_type": "aruba_osswitch",
-                    "ip": "10.15.40.14",
-                    "username": "username",
+                    "ip": "10.15.40.14", #Enter the Management IP address of the switch, through this it will take SSH.
+                    "username": "username", #Enter the local or TACACS/RADIUS username and password.
                     "password": "password",
-                    "global_delay_factor" : 2,
+                    "global_delay_factor" : 2, #This will keep a delay of 2 seconds for every command it will send it to the switch.
                                                         }
 
-net_connect = ConnectHandler(**aruba_osswitch)
+net_connect = ConnectHandler(**aruba_osswitch) #Taking SSH of the switch
 
+#In the Aruba switches there are multiple prompts that are asked after enabling snmpv3.
+#So accordingly I have set the commands to send it to the switch.
+#This set of commands will create an snmpv3 user 'initial' with password 'initial'.
+#You can change this later
 
 commands = [
             'configure terminal',
