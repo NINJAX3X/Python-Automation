fb7e036a2e94019659b1bf7d142f14b55dab8a7c
NINJAX3X <125155323+NINJAX3X@users.noreply.github.com>
2023-09-23 17:37:06 +0530

    Update and rename Aruba all config script.py to HPE Aruba 2530F and 2930F configuration template.py

--- HPE Aruba 2530F and 2930F configuration template.py
+++ HPE Aruba 2530F and 2930F configuration template.py
@@ -4,26 +4,29 @@ import time
 
 print("\n")
 
-print("--------------------------This script is only valid for Aruba OS Switches---------------------------")
+print("-----------------------This script is only valid for Aruba OS Switches only-------------------------")
 print("--------------------------Kindly follow the script instructions as it is----------------------------")
 
 time.sleep(2)
+
 print("\n")
 
 print("---------------------Before running script configure MGMT vlan, password manager,------------------------")
 print("----------------IP address and port 1 which connects to PC as an access port in MGMT vlan----------------")
 
 time.sleep(2)
+
 print("\n")
 
-print("Commands to configure the above is as following: ""\n""1) Configure terminal""\n""2) vlan (MGMT vlan ID)""\n""3) untagged 1""\n""4) ip address (x.x.x.x x.x.x.x)")
+print("Commands to configure the above is as following: ""\n""1) Configure terminal""\n""2) vlan (MGMT vlan ID)""\n""3) untagged 1""\n""4) ip address (10.15.x.x 255.255.255.128)")
 
 time.sleep(1)
+
 print("\n")
 
 value = input("Have you configured (yes/no)?:  ")
 
-if value == "yes":
+if value == "yes" or value == "y":
     print("Executing Script...")
 else:
     print("Exiting...")
@@ -59,23 +62,25 @@ while True:
     if 2 <= int(f_o) <= 126:
         print ("Gateway will be 1")
         x = '.1'
+        mgmt = '01'
         break
     elif 128 <= int(f_o) <= 254:
         print ("Gatewy will be 129")
         x = '.129'
+        mgmt = '02'
         break
     else:
         print("Define a valid octet between (2-254)")
 
-def_gat = 'x.x.'+str(t_o)+str(x)
+def_gat = '10.15.'+str(t_o)+str(x)
 
-ip = '{}.{}.{}.{}'.format(x,x,t_o,f_o)
+ip = '{}.{}.{}.{}'.format(10,15,t_o,f_o)
 
 aruba_osswitch = {
     "device_type": "aruba_osswitch",
     "ip": ip,
-    "username": "usernamet",
-    "password": "password",     
+    "username": "username",
+    "password": "Password",     
 }
 
 net_connect = ConnectHandler(**aruba_osswitch)
@@ -84,12 +89,14 @@ print("\n")
 
 net_connect.send_command(input(">>>>>>>>>>>>>>>>>>>>>>Press_Enter>>>>>>>>>>>>>>>>>>>>>>"))
 
+print("\n")
+
 time.sleep(2)
 
 config_commands1 = ['configure terminal',
                     'hostname '+str(Host),
                     'banner motd / ',
-                    banner text should be here with proper syntax
+                    #Enter the banner message of the day.
                     '/',
                     'console idle-timeout 1800',
                     'console idle-timeout serial-usb 1800',
@@ -97,20 +104,18 @@ config_commands1 = ['configure terminal',
                     'dhcp-snooping authorized-server x.x.x.x',
                     'dhcp-snooping authorized-server x.x.x.x',
                     'dhcp-snooping authorized-server x.x.x.x',
-                    'logging syslog server ip',
-                    'timesync sntp',
+                    'logging x.x.x.x',
+                    'timesync ntp',
                     'ntp server x.x.x.x',
-                    'time timezone 330',
                     'ip default-gateway '+str(def_gat),
                     'no telnet-server',
-                    'tacacs-server host x.x.x.x key password',
+                    'tacacs-server host x.x.x.x key Password',
                     'aaa authentication login privilege-mode',
                     'aaa authentication ssh login tacacs local',
                     'aaa authentication ssh enable tacacs local',
-                    'snmp-server contact name location '+str(loc), 
-                    'snmpv3 targetaddress xxx params xxx x.x.x.x',
-                    'snmpv3 group managerpriv user name sec-model ver3',
-                    'snmpv3 targetaddress name params all x.x.x.x',
+                    'snmp-server contact username location '+str(loc),
+                    'snmpv3 group managerpriv user username sec-model ver3',
+                    'snmpv3 targetaddress user params all x.x.x.x',
                     'spanning-tree',
                     'spanning-tree 1-8 bpdu-protection',
                     'write memory',
@@ -132,52 +137,61 @@ while True:
         net_connect.send_config_set(config_commands1[:41])
         untagged = '2-8'
         tagged = '9-10'
+        break
     elif ports == '24':
         net_connect.send_config_set(config_commands1[:45])
         untagged = '2-24'
         tagged = '25-28'
+        break
     elif ports == '48':
         net_connect.send_config_set(config_commands1)
         untagged = '2-48'
         tagged = '49-52'
+        break
     else:
         print("Enter valid number of ports which are mentioned (8/24/48):  ")
 
 net_connect.disconnect()
 
+print("Configuring VLANs...")
+
+print("\n")
+
 time.sleep(3)
 
-b_vlan = "{}{}".format(a,xx)
+b_vlan = "{}{}".format(a,11)
 
-c_vlan = "{}{}".format(a,xx)
+c_vlan = "{}{}".format(a,12)
 
-d_vlan = "{}{}".format(a,xx)
+d_vlan = "{}{}".format(a,21)
 
-e_vlan = "{}{}".format(a,xx)
+e_vlan = "{}{}".format(a,22)
 
-f_vlan = "{}{}".format(a,xx)
+f_vlan = "{}{}".format(a,31)
 
-g_vlan = "{}{}".format(a,xx)
+g_vlan = "{}{}".format(a,32)
 
-h_vlan = "{}{}".format(a,xx)
+h_vlan = "{}{}".format(a,41)
 
-i_vlan = "{}{}".format(a,xx)
+i_vlan = "{}{}".format(a,42)
 
-j_vlan = "{}{}".format(a,xx)
+j_vlan = "{}{}".format(a,51)
 
-k_vlan = "{}{}".format(a,xx)
+k_vlan = "{}{}".format(a,52)
 
-l_vlan = "{}{}".format(a,xx)
+l_vlan = "{}{}".format(a,61)
 
-m_vlan = "{}{}".format(a,xx)
+m_vlan = "{}{}".format(a,62)
 
-n_vlan = "{}{}".format(a,xx)
+n_vlan = "{}{}".format(a,81)
 
-o_vlan = "{}{}".format(a,xx)
+o_vlan = "{}{}".format(a,82)
 
-p_vlan = "{}{}".format(a,xx)
+p_vlan = "{}{}".format(a,91)
 
-q_vlan = "{}{}".format(a,xx)
+q_vlan = "{}{}".format(a,92)
+
+r_vlan = "{}{}".format(a,mgmt)
 
 time.sleep(2)
 
@@ -185,70 +199,73 @@ net_connect = ConnectHandler(**aruba_osswitch)
 
 config_commands2 = ['configure terminal',
                     'vlan '+str(b_vlan),
-                    'name DHCPxx',
+                    'name DHCP11',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(c_vlan),
-                    'name DHCPxx',
+                    'name DHCP12',
                     'untagged '+str(untagged),
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(d_vlan),
-                    'name DHCPxx',
+                    'name DHCP21',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(e_vlan),
-                    'name DHHCPxx',
+                    'name DHHCP22',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(f_vlan),
-                    'name DHCPxx',
+                    'name DHCP31',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(g_vlan),
-                    'name DHCPxx',
+                    'name DHCP32',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(h_vlan),
-                    'name DHCPxx',
+                    'name DHCP41',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(i_vlan),
-                    'name DHCPxx',
+                    'name DHCP42',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan ' +str(j_vlan),
-                    'name DHCPxx',
+                    'name PRINTER51',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(k_vlan),
-                    'name DHCPxx',
+                    'name PUNCHING52',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(l_vlan),
-                    'name DHCPxx',
+                    'name FIX61',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(m_vlan),
-                    'name DHCPxx',
+                    'name FIX62',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(n_vlan),
-                    'name DHCPxx',
+                    'name RF81',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(o_vlan),
-                    'name DHCPxx',
+                    'name RF82',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(p_vlan),
-                    'name DHCPxx',
+                    'name CCTV91',
                     'tagged '+str(tagged),
                     'exit',
                     'vlan '+str(q_vlan),
-                    'name DHCPxx',
+                    'name CCTV92',
                     'tagged '+str(tagged),
                     'exit',
+                    'vlan '+str(r_vlan),
+                    'name MGMT',
+                    'tagged '+str(tagged),
                     'exit'
 
                                              ]
@@ -259,6 +276,29 @@ time.sleep(2)
 
 net_connect.disconnect()
 
+print("Configuring SNMPv3...")
+
 print("\n")
 
-print("CONFIG DONE....")
+net_connect = ConnectHandler(**aruba_osswitch)
+
+commands = [
+            'configure terminal',
+            'snmpv3 enable',
+            'initial',
+            'initial',
+            'n',
+            'y',
+            'snmpv3 only',
+            'snmpv3 user username auth md5 auth password priv aes privacy password',
+            'no snmpv3 user initial',
+            'allow-unsupported-transceiver',
+            'y',
+            'exit',
+                                                            ]
+
+output = net_connect.send_multiline_timing(commands, multiline = True)
+
+print("Configuration has been done...")
+
+net_connect.disconnect()
